1816cec4564db679196798608a93aa7a3a165fdc
Thomas <evers.thomas@gmail.com>
2020-06-18 11:18:17 +0200

    Update script_styles.php

--- inc/functions/script_styles.php
+++ inc/functions/script_styles.php
@@ -1,16 +1,19 @@
 <?php
-// enqueue styles and scripts
-function add_naven_scripts() {
-  wp_enqueue_style( 'style', get_stylesheet_uri() );
+/**
+ * Enqueue scripts and styles.
+ */
+function mind_nature_scripts() {
+	wp_enqueue_style( 'mind_nature-style', get_stylesheet_uri(), array(), mind_nature_VERSION );
+	wp_style_add_data( 'mind_nature-style', 'rtl', 'replace' );
   
   wp_deregister_script( 'jquery' );
-    // if you have problems with plugins,uncomment this latest version of jquery
-    wp_register_script( 'jquery', "https://code.jquery.com/jquery-3.4.1.min.js", array(), '3.4.1',true );
- 
-  wp_enqueue_script( 'navigation', get_template_directory_uri() . '/js/navigation.js', array ( 'jquery' ), 1.1, true);
- 
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-      wp_enqueue_script( 'comment-reply' );
-    }
+  // if you have problems with plugins,uncomment this latest version of jquery
+  wp_register_script( 'jquery', "https://code.jquery.com/jquery-3.4.1.min.js", array(), '3.4.1',true );
+
+	wp_enqueue_script( 'mind_nature-navigation', get_template_directory_uri() . '/js/navigation.js', array(), mind_nature_VERSION, true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
 }
-add_action( 'wp_enqueue_scripts', 'add_naven_scripts' );
+add_action( 'wp_enqueue_scripts', 'mind_nature_scripts' );
